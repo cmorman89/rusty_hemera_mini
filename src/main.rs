df78bc1f977b54d40aa6ba2cca946b2728423c96
Charles Morman <github@cmorman.com>
2025-01-09 14:50:07 -0700

    feat: cleanup, slight optimizations

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,17 @@
-// extern crate image;
-// extern crate ndarray;
-use image::{Rgba, AnimationDecoder, Frame, ImageBuffer};
-use ndarray::{Array2, Array3};
-use std::{thread, time, vec};
-use std::collections::VecDeque;
-use std::fs::File;
-use image::codecs::gif::GifDecoder;
-use std::io::{BufReader, self, Write};
+use ndarray::Array3;
+use std::io::{self, Write};
 use ffmpeg_next::{self as ffmpeg, frame};
 
-fn frame_sleep(fps: &u32, frame_start: time::Instant) {
-    /// Sleeps the thread to maintain a target frame rate
-    /// 
-    /// # Arguments
-    /// - `fps` - The target frames per second
-    /// - `frame_start` - The time the frame started
-    /// 
-    /// # Returns
-    /// - None
-    
-    // Calculate the frame duration based on the target frames per second
-    let frame_duration = time::Duration::from_secs(1) / fps.clone();
-    // Calculate the elapsed time since the frame started
-    let elapsed_time = frame_start.elapsed();
-    // If the frame finished rendering faster than the target frame duration, sleep the thread
-    if elapsed_time < frame_duration {
-        thread::sleep(frame_duration - elapsed_time);
-    }
-}
+/// Prints an RGB array to the terminal
+/// 
+/// # Arguments
+/// - `rgb_array` - The RGB array to print
+/// - `frame_buffer` - The buffer to write the frame to
+/// 
+/// # Returns
+/// - None
+fn print_rgb_array(rgb_array: &Array3<u8>, frame_buffer: &mut Vec<u8>, trim_stride: bool) {
 
-
-fn print_rgb_array(rgb_array: &Array3<u8>, frame_buffer: &mut Vec<u8>) {
-    /// Prints an RGB array to the terminal
-    /// 
-    /// # Arguments
-    /// - `rgb_array` - The RGB array to print
-    /// - `frame_buffer` - The buffer to write the frame to
-    /// 
-    /// # Returns
-    /// - None
-    
     // Clear the frame buffer from the previous frame
     frame_buffer.clear();
 
@@ -67,67 +39,61 @@ fn print_rgb_array(rgb_array: &Array3<u8>, frame_buffer: &mut Vec<u8>) {
     // Move the cursor to the top left corner
     frame_buffer.extend_from_slice(to_origin);
     let (h, w, _) = rgb_array.dim();
-    for y in 0..h {
-        // Only print even rows due to the stacked vertical pixels sharing one character cell
-        if y % 2 == 0 && y < h - 2 {
-            for x in 0..w {
-                // Extract the RGB values from the array
-                // Foreground
-                fg_r = rgb_array[[y, x, 0]];
-                fg_g = rgb_array[[y, x, 1]];
-                fg_b = rgb_array[[y, x, 2]];
-                // Background
-                bg_r = rgb_array[[y + 1, x, 0]];
-                bg_g = rgb_array[[y + 1, x, 1]];
-                bg_b = rgb_array[[y + 1, x, 2]];
+    // The stride adds 4 bytes to the end of the each row of the video frame
+    // This is due to the padding added by the ffmpeg software scaler
+    // Slicing the ndarray will cause memory reallocation and slow down the rendering
+    // Instead, we simply do not iterate over the last 4 columns of the frame
+    let w = if trim_stride { w - 32 } else { w };
+    // Only print even rows due to the stacked vertical pixels sharing one character cell
+    for y in (0..h).step_by(2) {
+        for x in 0..w {
+            // Extract the RGB values from the array
+            // Foreground
+            fg_r = rgb_array[[y, x, 0]];
+            fg_g = rgb_array[[y, x, 1]];
+            fg_b = rgb_array[[y, x, 2]];
+            // Background
+            bg_r = rgb_array[[y + 1, x, 0]];
+            bg_g = rgb_array[[y + 1, x, 1]];
+            bg_b = rgb_array[[y + 1, x, 2]];
 
-                // Generate the foreground ANSI escape sequence
-                frame_buffer.extend_from_slice(fg_ansi);
-                frame_buffer.extend_from_slice(&u8_to_bytes(fg_r));
-                frame_buffer.extend_from_slice(ansi_sep);
-                frame_buffer.extend_from_slice(&u8_to_bytes(fg_g));
-                frame_buffer.extend_from_slice(ansi_sep);
-                frame_buffer.extend_from_slice(&u8_to_bytes(fg_b));
-                frame_buffer.extend_from_slice(ansi_end);
+            // Generate the foreground ANSI escape sequence
+            frame_buffer.extend_from_slice(fg_ansi);
+            frame_buffer.extend_from_slice(&u8_to_bytes(fg_r));
+            frame_buffer.extend_from_slice(ansi_sep);
+            frame_buffer.extend_from_slice(&u8_to_bytes(fg_g));
+            frame_buffer.extend_from_slice(ansi_sep);
+            frame_buffer.extend_from_slice(&u8_to_bytes(fg_b));
+            frame_buffer.extend_from_slice(ansi_end);
 
-                // Generate the background ANSI escape sequence
-                frame_buffer.extend_from_slice(bg_ansi);
-                frame_buffer.extend_from_slice(&u8_to_bytes(bg_r));
-                frame_buffer.extend_from_slice(ansi_sep);
-                frame_buffer.extend_from_slice(&u8_to_bytes(bg_g));
-                frame_buffer.extend_from_slice(ansi_sep);
-                frame_buffer.extend_from_slice(&u8_to_bytes(bg_b));
-                frame_buffer.extend_from_slice(ansi_end);
+            // Generate the background ANSI escape sequence
+            frame_buffer.extend_from_slice(bg_ansi);
+            frame_buffer.extend_from_slice(&u8_to_bytes(bg_r));
+            frame_buffer.extend_from_slice(ansi_sep);
+            frame_buffer.extend_from_slice(&u8_to_bytes(bg_g));
+            frame_buffer.extend_from_slice(ansi_sep);
+            frame_buffer.extend_from_slice(&u8_to_bytes(bg_b));
+            frame_buffer.extend_from_slice(ansi_end);
 
-                // Print the character
-                frame_buffer.extend_from_slice(&print_char);
-            }
-            frame_buffer.extend_from_slice(b"\x1b[0m\n");
+            // Print the character
+            frame_buffer.extend_from_slice(&print_char);
         }
+        frame_buffer.extend_from_slice(b"\x1b[0m\n");
     }
     handle.write_all(&frame_buffer).unwrap();
 }
 
-fn initialize_rasterizer() {
-    /// Initializes the rasterizer by preallocating buffers, variables, and precalculating byte-
-    /// representations of ANSI escape sequences.
-    
-    // Clear the screen
-    print!("\x1b[2J");
-    // Hide the cursor
-    print!("\x1b[?25l");
-}
 
 
+/// Converts a u8 value to a 3 element array of ASCII bytes
+/// Avoids overhead of heap allocation for string conversion
+/// 
+/// # Arguments
+/// - `u8_val` - The u8 value to convert
+/// 
+/// # Returns
+/// - A 3 element array of ASCII bytes
 fn u8_to_bytes(u8_val: u8) -> [u8; 3] {
-    /// Converts a u8 value to a 3 element array of ASCII bytes
-    /// Avoids overhead of heap allocation for string conversion
-    /// 
-    /// # Arguments
-    /// - `u8_val` - The u8 value to convert
-    /// 
-    /// # Returns
-    /// - A 3 element array of ASCII bytes
     
     let mut bytes: [u8; 3] = [0; 3];
     bytes[0] = u8_val / 100 + b'0';
@@ -136,65 +102,13 @@ fn u8_to_bytes(u8_val: u8) -> [u8; 3] {
     bytes
 }
 
-fn gif_to_deque(image_path: &str) -> VecDeque<Array3<u8>> {
-    /// Converts a GIF file to a deque of RGB arrays
-    /// 
-    /// # Arguments
-    /// - `image_path` - The path to the GIF file
-    /// 
-    /// # Returns
-    /// - A deque of RGB arrays
-    
-    // Create a deque to store the frames
-    let mut frame_deque: VecDeque<Array3<u8>> = VecDeque::new();
-
-    // Open the GIF file and get the frames
-    let gif_file = BufReader::new(File::open(image_path).expect("\n===========\nIMAGE NOT FOUND!\n===========\n"));
-    let decoder = GifDecoder::new(gif_file).unwrap();
-    let frames = decoder.into_frames();
-    let frames = frames.collect_frames().expect("\n===========\nFAILED TO COLLECT FRAMES!\n===========\n");
-    
-    // Extract frames from the GIF
-    for frame in frames{
-        let gif_rgb_array = process_frame(&frame);
-        // Add the RGB array to the deque
-        frame_deque.push_back(gif_rgb_array);
-    }
-    // Return the deque of RGB array frames
-    frame_deque
-}
-
-fn process_frame(frame: &Frame) -> Array3<u8> {
-    /// Processes a GIF frame into an RGB array
-    /// 
-    /// # Arguments
-    /// - `frame` - The GIF frame to process
-    /// 
-    /// # Returns
-    /// - An RGB array
-    
-    // Get the buffer from the frame
-    let buffer: &ImageBuffer<Rgba<u8>, Vec<u8>> = &frame.buffer();
-    // Get the dimensions of the buffer
-    let (w, h) = buffer.dimensions();
-    // Create an RGB array to store the pixel values
-    let mut rgb_array: Array3<u8> = Array3::zeros((h as usize, w as usize, 3));
-    for (x, y, pixel) in buffer.enumerate_pixels() {
-        let (r, g, b, _): (u8, u8, u8, u8) = pixel.0.into();
-        rgb_array[[y as usize, x as usize, 0]] = r;
-        rgb_array[[y as usize, x as usize, 1]] = g;
-        rgb_array[[y as usize, x as usize, 2]] = b;
-    }
-    // Return the RGB array
-    rgb_array
-}
 
 fn main() {
     // Initialize ffmpeg
     ffmpeg::init().expect("Failed to initialize ffmpeg");
 
     // Open the video file and get the input context
-    let video_path = "asset/planet-earth.mp4";
+    let video_path = "asset/small_2.mp4";
     let mut ictx = ffmpeg::format::input(&video_path).expect("Failed to open input file");
 
     // Get the index of the video stream in the video file
@@ -205,7 +119,7 @@ fn main() {
 
     // Get the video stream parameters using the video stream index;
     // and create a decoder from the parameters
-    let mut decoder_context = ffmpeg::codec::context::Context::from_parameters(
+    let decoder_context = ffmpeg::codec::context::Context::from_parameters(
         ictx
             .stream(video_stream_index).expect("Failed to get video stream")
             .parameters(),
@@ -219,23 +133,24 @@ fn main() {
         ffmpeg::util::format::Pixel::RGB24,
         decoder.width(),
         decoder.height(),
-        // Uncomment for bilinear scaling
         ffmpeg::software::scaling::flag::Flags::BILINEAR
-        // Uncomment for nearest neighbor scaling
-        // ffmpeg::software::scaling::flag::Flags::NEAREST,
     ).expect("Failed to create video to RGB context");
-
     let mut frame_index = 0;
-
+    let mut frame_buffer: Vec<u8> = Vec::new();
+    let mut frame_buffer_is_initialized = false;
     for (stream, packet) in ictx.packets() {
         if stream.index() == video_stream_index {
             // If the packet is a video packet, send it to the decoder
             decoder.send_packet(&packet).expect("Failed to send packet to decoder");
             let h = decoder.height() as usize;
-            let w = (decoder.width() as usize + 4) * 3;
+            let w = decoder.width() as usize;
+            let stride_w = (w + 4) * 3;
             let mut input_frame = ffmpeg::util::frame::Video::empty();
-            let byte_estimate: usize = h * w * 20;
-            let mut frame_buffer: Vec<u8> = Vec::with_capacity(byte_estimate);
+            let byte_estimate: usize = h * stride_w * 20;
+            if !frame_buffer_is_initialized {
+                frame_buffer = Vec::with_capacity(byte_estimate);
+                frame_buffer_is_initialized = true;
+            }
         
             // While the decoder has frames to process, process them
             while decoder.receive_frame(&mut input_frame).is_ok() {
@@ -245,40 +160,16 @@ fn main() {
                 video_to_rgb.run(&input_frame, &mut rgb_frame).expect( "Failed to convert video frame to RGB");
                 // Get the pixel data from the RGB frame
                 // let w = rgb_frame.stride(0);
-                let stride = rgb_frame.stride(0);
                 let pixel_data = rgb_frame.data(0);
                 // Turn the pixel data into a vector
-                println!("Stride: {}", stride);
-                println!("Frame {}: Pixel data length: {} vs h ({}) * w ({}) = {}", &frame_index, pixel_data.len(), &h, &w, h * w);
                 let w = rgb_frame.stride(0);
                 let rgb_ndarray = Array3::from_shape_vec((h, w / 3, 3), pixel_data.to_vec()).expect("Failed to create ndarray");
                 
                 frame_index += 1;
-                print_rgb_array(&rgb_ndarray, &mut frame_buffer);
+                print_rgb_array(&rgb_ndarray, &mut frame_buffer, true);
 
             }
         }
     }
 
-
-    // Path to the GIF file
-    let gif_path: &str = "asset/planet-earth.gif";
-    // Set the target frames per second
-    let fps: u32 = 30;
-
-    // Convert the GIF to a deque of RGB arrays
-    let frame_deque = gif_to_deque(gif_path);
-    let rgb_array = frame_deque.front().unwrap();
-    let (h, w, _) = rgb_array.dim();
-    let byte_estimate: usize = h * w * 20;
-    let mut frame_buffer: Vec<u8> = Vec::with_capacity(byte_estimate);
-
-    print!("\x1b[0;0H");
-    loop {
-        for frame in frame_deque.iter() {
-            let start_time = time::Instant::now();
-            print_rgb_array(&frame, &mut frame_buffer);
-            frame_sleep(&fps, start_time);
-        }
-    }
 }

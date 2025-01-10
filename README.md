420e21c893d3f69abe31dd6a454e7396cf397dc6
Charles Morman <github@cmorman.com>
2025-01-09 19:02:01 -0700

    feat: cleaned up and optimized code, made it easier to use, and update readme

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ GIFs and videos are being used as input material for testing -- hence the lack o
 
 ### Note on code quality:
 
-I am learning Rust as I go, so this code may not be idiomatic Rust -- or even correct Rust. But, I've always found the best way to learn is to bite off more than you can chew...and chew like hell until it makes sense. :)
+I am learning Rust as I go, so this code may not be idiomatic Rust -- or even correct Rust. But, I've always found the best way to learn is to bite off more than you can chew...and chew like hell until it makes sense. 🤯
 
 This repo is:
 - A learning tool
@@ -25,4 +25,82 @@ This repo is:
 
 ## Progress:
 
-So far, I am quite happy: [Check out the demos (based on a slower version)!](https://www.youtube.com/playlist?list=PLvkXEUKaigSyHm_Q2-Cmmdko0IKtBOLpU)
+- 1-9-2025:
+> I am bouncing back and forth between this, two client websites, and a few other projects. I'd like to spend more time on this, but for now I am happy with the progress. Videos are relatively smooth, considering the lack of any major optimization to the amount of characters needing to be printed to the terminal.
+
+- 1-7-2025:
+> So far, I am quite happy: [Check out the demos (based on a slower version)!](https://www.youtube.com/playlist?list=PLvkXEUKaigSyHm_Q2-Cmmdko0IKtBOLpU)
+
+
+## Basic Usage:
+1. Clone the repo
+    > ```bash
+    > git clone https://github.com/cmorman89/rusty_hemera_mini
+    > ```
+2. Change to the project directory
+    > ```bash
+    > cd rusty_hemera_mini
+    > ```
+3. Install the dependencies
+    > Install Rust via RustUp (Linux): 
+    > ```bash
+    > curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+    > ```
+
+    > Install Rust via RustUp (Windows):
+    > ```powershell
+    > Invoke-WebRequest https://win.rustup.rs -OutFile rustup-init.exe; .\rustup-init.exe
+    > ```
+
+    > Install ffmpeg-devel (or equivalent) via your package manager
+    > - I will provide better instructions for this in the future
+
+3. Change the video_path in `main.rs` to the path of the video you want to play
+    > ```rust
+    > // Path to the video file
+    > let video_path = "asset/small_2.mp4";
+    > ```
+
+4. Run the program
+    > ```bash
+    > cargo run --release
+    > ```
+5. Enjoy the video!
+
+> [!NOTE]
+> - Be sure to zoom out of the terminal to see the full video!
+> - Use Ctrl+C to stop the video
+> - Try to limit the input video to 400x200 @ 30fps for the best results
+
+## Dependencies:
+
+### Rust Cargo Packages:
+  - ffmpeg-next
+  - ndarray
+
+### System Dependencies:
+  - ffmpeg-devel (or equivalent)
+  > [!WARNING]
+  > This was a bit of a pain to install on Fedora. I had to install the RPM Fusion repos and then install ffmpeg-devel with flags set to allow erasing existing packages due to version issues. I am not sure if this is the best way to do it, but it worked for me.
+  >
+  > I'd Google it instead of following my advice on this one.
+
+## To Do Ideas:
+- Frame-by-frame change detection:
+    - Delta buffer to only update changed cells
+    - Run buffers for ANSI escape caching
+    - Consider quantiziation methods to limit small variations in color
+- Parallelize the rendering process
+    - Use Rayon for parallelization
+    - Use SIMD for pixel processing
+    - Split the image into chunks for parallel processing?
+    - Split the ndarray into chunks for parallel processing?
+- ~Check if ndarray is the right data structure for holding RGB values~
+    - Seem to be the best option for now, as tracking a Vec index was visibly slower
+- Look at SIMD for pixel processing of u8 -> bytes
+    - Does ndarray automatically use SIMD? If not, how to enable it?
+- Look at mapping u8 -> bytes
+    - Currently moved to inline arithmetic
+    - Maybe use a macro, though I have heard they are complex to make in Rust
+    - Whole-ndarray operations to convert to bytes in one go?
+- Learn error handling in Rust.expect(😭)
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,28 @@
-use ndarray::Array3;
+use ndarray::{ArrayView3};
 use std::io::{self, Write};
-use ffmpeg_next::{self as ffmpeg, frame};
+use ffmpeg_next::{self as ffmpeg};
 
 /// Prints an RGB array to the terminal
 /// 
 /// # Arguments
 /// - `rgb_array` - The RGB array to print
 /// - `frame_buffer` - The buffer to write the frame to
+/// - `trim_stride` - Whether to trim the excess stride from the right side of the frame
 /// 
 /// # Returns
 /// - None
-fn print_rgb_array(rgb_array: &Array3<u8>, frame_buffer: &mut Vec<u8>, trim_stride: bool) {
+fn print_rgb_array(rgb_array: &ArrayView3<u8>, frame_buffer: &mut Vec<u8>, trim_stride: bool) {
 
     // Clear the frame buffer from the previous frame
     frame_buffer.clear();
 
-    // Get the stdout handle
-    let stdout = io::stdout();
-    let mut handle = stdout.lock();
+    // Get the dimensions of the RGB array
+    let (h, w, _) = rgb_array.dim();
+
+    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    // TO-DO: Fix something weird with the video on the right column
+    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    let w = if trim_stride { w - 32 } else { w };
 
     // Preallocate the pixel values
     let mut fg_r: u8;
@@ -34,17 +39,12 @@ fn print_rgb_array(rgb_array: &Array3<u8>, frame_buffer: &mut Vec<u8>, trim_stri
     let ansi_sep: &[u8; 1] = b";";
     let ansi_end: &[u8; 1] = b"m";
     let print_char: &[u8] = "▀".as_bytes();
+    let ansi_reset: &[u8; 5] = b"\x1b[0m\n";
 
     // Start printing:
     // Move the cursor to the top left corner
     frame_buffer.extend_from_slice(to_origin);
-    let (h, w, _) = rgb_array.dim();
-    // The stride adds 4 bytes to the end of the each row of the video frame
-    // This is due to the padding added by the ffmpeg software scaler
-    // Slicing the ndarray will cause memory reallocation and slow down the rendering
-    // Instead, we simply do not iterate over the last 4 columns of the frame
-    let w = if trim_stride { w - 32 } else { w };
-    // Only print even rows due to the stacked vertical pixels sharing one character cell
+     // Only print even rows due to the stacked vertical pixels sharing one character cell
     for y in (0..h).step_by(2) {
         for x in 0..w {
             // Extract the RGB values from the array
@@ -58,74 +58,90 @@ fn print_rgb_array(rgb_array: &Array3<u8>, frame_buffer: &mut Vec<u8>, trim_stri
             bg_b = rgb_array[[y + 1, x, 2]];
 
             // Generate the foreground ANSI escape sequence
+            //
+            // - The format is: 
+            //   - Foreground: \x1b[38;2;{r};{g};{b}m
+            //   - Background: \x1b[48;2;{r};{g};{b}m
+            //
+            // - The mathmatical operations are used to convert the u8 to bytes without using a string
+            //   (A string would require a heap allocation for every pixel color *channel* -- too slow)
+            //
+            // - The math was moved inline to avoid the overhead of a function call, again for every pixel
+            //   color *channel* (3 per pixel, 6 per loop iteration, or 7,200,000 calls per second for a 
+            //   400*200 video at 30fps. Ouch.)
             frame_buffer.extend_from_slice(fg_ansi);
-            frame_buffer.extend_from_slice(&u8_to_bytes(fg_r));
+            frame_buffer.extend_from_slice(&[fg_r / 100 + b'0', (fg_r % 100) / 10 + b'0', fg_r % 10 + b'0']);
             frame_buffer.extend_from_slice(ansi_sep);
-            frame_buffer.extend_from_slice(&u8_to_bytes(fg_g));
+            frame_buffer.extend_from_slice(&[fg_g / 100 + b'0', (fg_g % 100) / 10 + b'0', fg_g % 10 + b'0']);
             frame_buffer.extend_from_slice(ansi_sep);
-            frame_buffer.extend_from_slice(&u8_to_bytes(fg_b));
+            frame_buffer.extend_from_slice(&[fg_b / 100 + b'0', (fg_b % 100) / 10 + b'0', fg_b % 10 + b'0']);
             frame_buffer.extend_from_slice(ansi_end);
 
             // Generate the background ANSI escape sequence
             frame_buffer.extend_from_slice(bg_ansi);
-            frame_buffer.extend_from_slice(&u8_to_bytes(bg_r));
+           frame_buffer.extend_from_slice(&[bg_r / 100 + b'0', (bg_r % 100) / 10 + b'0', bg_r % 10 + b'0']);
             frame_buffer.extend_from_slice(ansi_sep);
-            frame_buffer.extend_from_slice(&u8_to_bytes(bg_g));
+            frame_buffer.extend_from_slice(&[bg_g / 100 + b'0', (bg_g % 100) / 10 + b'0', bg_g % 10 + b'0']);
             frame_buffer.extend_from_slice(ansi_sep);
-            frame_buffer.extend_from_slice(&u8_to_bytes(bg_b));
+            frame_buffer.extend_from_slice(&[bg_b / 100 + b'0', (bg_b % 100) / 10 + b'0', bg_b % 10 + b'0']);
             frame_buffer.extend_from_slice(ansi_end);
 
-            // Print the character
-            frame_buffer.extend_from_slice(&print_char);
+            // Append the block character
+            frame_buffer.extend_from_slice(print_char);
         }
-        frame_buffer.extend_from_slice(b"\x1b[0m\n");
+        // Reset formatting and move to the next line
+        frame_buffer.extend_from_slice(ansi_reset);
     }
-    handle.write_all(&frame_buffer).unwrap();
-}
-
-
-
-/// Converts a u8 value to a 3 element array of ASCII bytes
-/// Avoids overhead of heap allocation for string conversion
-/// 
-/// # Arguments
-/// - `u8_val` - The u8 value to convert
-/// 
-/// # Returns
-/// - A 3 element array of ASCII bytes
-fn u8_to_bytes(u8_val: u8) -> [u8; 3] {
-    
-    let mut bytes: [u8; 3] = [0; 3];
-    bytes[0] = u8_val / 100 + b'0';
-    bytes[1] = (u8_val % 100) / 10 + b'0';
-    bytes[2] = u8_val % 10 + b'0';
-    bytes
+    io::stdout().lock().write_all(&frame_buffer).unwrap();
 }
 
 
 fn main() {
+
+    // HOW TO USE:
+    // 1. Change the video_path to the path of the video you want to play
+    // 2. Change to the project directory if you are not already there
+    // 3. Run the program with `cargo run --release`
+    // 4. Enjoy the video!
+    //
+    // Tips:
+    // - Be sure to zoom out of the terminal to see the full video!
+    // - Use Ctrl+C to stop the video
+    // - Try to limit the input video to 400x200 @ 30fps for the best results
+    // 
+    // Dependencies:
+    // - Rust Cargo Packages:
+    //   - ffmpeg-next
+    //   - ndarray
+    // - System Dependencies:
+    //   - ffmpeg-devel (or equivalent)
+    
+    // Path to the video file
+    let video_path = "asset/small_2.mp4";
+
     // Initialize ffmpeg
     ffmpeg::init().expect("Failed to initialize ffmpeg");
 
-    // Open the video file and get the input context
-    let video_path = "asset/small_2.mp4";
+    // Open the video file to get the input context
     let mut ictx = ffmpeg::format::input(&video_path).expect("Failed to open input file");
 
-    // Get the index of the video stream in the video file
+    // Get the index of the correct (video) stream in the input video file
     let video_stream_index = ictx
         .streams()
         .best(ffmpeg::media::Type::Video).expect("Failed to get best video stream")
         .index();
 
-    // Get the video stream parameters using the video stream index;
-    // and create a decoder from the parameters
+    // Get the context from the video stream
     let decoder_context = ffmpeg::codec::context::Context::from_parameters(
         ictx
             .stream(video_stream_index).expect("Failed to get video stream")
             .parameters(),
         ).expect("Failed to create decoder");
+    
+    // Create a video decoder from the decoder context
     let mut decoder = decoder_context.decoder().video().expect("Failed to create video decoder");
 
+    // Create a context to convert the video to RGB
     let mut video_to_rgb = ffmpeg::software::scaling::context::Context::get(
         decoder.format(),
         decoder.width(),
@@ -133,40 +149,71 @@ fn main() {
         ffmpeg::util::format::Pixel::RGB24,
         decoder.width(),
         decoder.height(),
-        ffmpeg::software::scaling::flag::Flags::BILINEAR
+        ffmpeg::software::scaling::flag::Flags::FAST_BILINEAR
     ).expect("Failed to create video to RGB context");
-    let mut frame_index = 0;
+    
+    // Track if the needed variables are initialized
+    let mut hemera_is_initialized = false;
+    
+    // Initialize the video frame and RGB frame outside of the loop
+    let mut input_frame = ffmpeg::util::frame::Video::empty();
+    let mut rgb_frame = ffmpeg::util::frame::Video::empty();
+    let mut rgb_view: ArrayView3<u8>;
+    let mut pixel_data: &[u8];
+    
+    // Create a buffer to store the printable frame data outside of the loop
     let mut frame_buffer: Vec<u8> = Vec::new();
-    let mut frame_buffer_is_initialized = false;
+
+    // Initialize dimensions outside of the loops
+    let mut h: usize = 0;
+    let mut w: usize;
+    let mut stride_w: usize;
+    let mut stride: usize;
+    let mut byte_estimate: usize;
+
+    // Track the current frame
+    let mut frame_index = 0;
+
+    // Loop through the packets in the input context
     for (stream, packet) in ictx.packets() {
+        // If the packet is a video packet, send it to the decoder
         if stream.index() == video_stream_index {
-            // If the packet is a video packet, send it to the decoder
             decoder.send_packet(&packet).expect("Failed to send packet to decoder");
-            let h = decoder.height() as usize;
-            let w = decoder.width() as usize;
-            let stride_w = (w + 4) * 3;
-            let mut input_frame = ffmpeg::util::frame::Video::empty();
-            let byte_estimate: usize = h * stride_w * 20;
-            if !frame_buffer_is_initialized {
+
+            // If the function has not initialized, initialize the variables
+            if !hemera_is_initialized {
+                // Get the actual height and width of the video
+                h = decoder.height() as usize;
+                w = decoder.width() as usize;
+                // Calculate the stride of the RGB frame
+                // (Since we dont yet know the stride of the RGB frame, we estimate it to be:
+                // width of the frame * three pixels (RGB) + 4 bytes of padding per row)
+                stride_w = (w + 4) * 3;
+
+                // // Create an empty video frame to store the input frame
+                // let mut input_frame = ffmpeg::util::frame::Video::empty();
+                byte_estimate = h * stride_w * 20;
                 frame_buffer = Vec::with_capacity(byte_estimate);
-                frame_buffer_is_initialized = true;
+                hemera_is_initialized = true;
             }
         
             // While the decoder has frames to process, process them
             while decoder.receive_frame(&mut input_frame).is_ok() {
-                // Create an empty RGB frame
-                let mut rgb_frame = ffmpeg::util::frame::Video::empty();
                 // Convert the video frame to an RGB frame
                 video_to_rgb.run(&input_frame, &mut rgb_frame).expect( "Failed to convert video frame to RGB");
-                // Get the pixel data from the RGB frame
-                // let w = rgb_frame.stride(0);
-                let pixel_data = rgb_frame.data(0);
-                // Turn the pixel data into a vector
-                let w = rgb_frame.stride(0);
-                let rgb_ndarray = Array3::from_shape_vec((h, w / 3, 3), pixel_data.to_vec()).expect("Failed to create ndarray");
-                
+
+                // Now that we have the RGB frame, we can view the pixel data and the actual stride
+                pixel_data = rgb_frame.data(0);
+                stride = rgb_frame.stride(0) / 3;
+
+                // Create a view of the pixel data instead of copying it (ArrayView3 vs Array3)
+                rgb_view = ArrayView3::from_shape((h, stride, 3), pixel_data).expect("Failed to view pixel data");
+
+                // Print the RGB view to the terminal
+                print_rgb_array(&rgb_view, &mut frame_buffer, true);
+
+                // Increment the frame index
                 frame_index += 1;
-                print_rgb_array(&rgb_ndarray, &mut frame_buffer, true);
 
             }
         }
